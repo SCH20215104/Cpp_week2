abd747afe75fe304bc8b80188ebc5b31e75fcaf4
하정원 <sch20215104@gmail.com>
2022-09-09 17:55:26 +0900

    1번

--- Week 1/week2.cpp
+++ Week 1/week2.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 using namespace std;
 
 int main() {
-	string Sentence;
-	int vowel;
+	char Alphabet, sentence;
+	int vowel = 0, consonants = 0;
 
 	cout << "영문자를 입력하고 Ctrl+Z를 치세요" << endl;
+
+	while (cin.eof() != true) {
+		cin >> Alphabet;
+		switch (Alphabet) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			vowel++;
+			break;
+		default:
+			consonants++;
+			break;
+
+		}
+	}
+	cout << "모음 : " << vowel << endl;
+	cout << "자음 : " << consonants << endl;
 }
